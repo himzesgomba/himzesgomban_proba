894bbfd0ac5e4b31bd62e6a9da95e21fc12b7953
himzesgomba <81706326+himzesgomba@users.noreply.github.com>
2021-04-06 17:09:01 +0200

    alap fileok

---
+++ cimerek.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    let selectorToCimer = {
+        '.cimer': [{ src: 'images/cimer1.jpg' },
+            { src: 'img/alpin/IMG_20171123_111642.jpg' },
+            { src: 'img/alpin/20180519_121042.jpg' },
+            { src: 'img/alpin/20180531_165857.jpg' },
+            { src: 'img/alpin/20180518_125041.jpg' },
+            { src: 'img/alpin/20180518_115047.jpg' },
+            { src: 'img/alpin/elotte-utana4.jpg' }
+        ],
+    };
+
+    Gallery(selectorToCimer);
+});
