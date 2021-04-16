f2eae3dd3950507b217c72c42b650dadc195aafd
agnesferenczi <65777475+agnesferenczi@users.noreply.github.com>
2021-04-16 17:36:19 +0200

    forms-os változtatások

---
+++ ajanlat.js
@@ -0,0 +1,11 @@
+const opovodai = document.querySelector('.contact-container .contactForm .inputBox .opovodai');
+const option = document.querySelector(' .contact-container .contactForm .inputBox .option');
+const ovoda = document.querySelectorAll('.contact-container .contactForm .inputBox .ovoda');
+const termek = document.querySelector('.contact-container .contactForm .inputBox .termek');
+const sajat = document.querySelector('.contact-container .contactForm .inputBox .sajat');
+
+
+ovoda.addEventListener('click', function() {
+    const opct = '0';
+    option.style.opacity = opct;
+});
